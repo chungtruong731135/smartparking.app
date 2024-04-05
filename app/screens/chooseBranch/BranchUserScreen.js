bed9e666bcb63c05c3ded116c134015d9704e221
chung <chungchung300702@gmail.com>
2024-04-05 17:35:49 +0700

    uppp

--- app/screens/chooseBranch/BranchUserScreen.js
+++ app/screens/chooseBranch/BranchUserScreen.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import API from '../../services/GlobalAPI';
-import { useSelector } from 'react-redux';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import API, { setAuthToken } from '../../services/GlobalAPI';
+import { useDispatch, useSelector } from 'react-redux';
 import { TD_Header } from '../../components';
 import { Colors } from '../../theme';
 import base64 from 'base-64';
 import { parseJwt } from '../../redux/global/Slice'
 import { useNavigation } from '@react-navigation/native';
-
+import { actions } from '../../redux/global/Actions'
 
 const BranchUserScreen = () => {
-    const token = useSelector((state) => state.global.accessToken); 
+    const token = useSelector((state) => state?.global?.accessToken); 
     const [branches, setBranches] = useState([]);
     const [branchDetails, setBranchDetails] = useState({});
     const navigation = useNavigation()
+    const dispatch = useDispatch();
 
-    let decodedToken = parseJwt(token);
-    let userId = decodedToken.uid;
+    let decodedToken = token ? parseJwt(token) : null;
+    let userId = decodedToken?.uid;
     
     const getBranchesForUser = async () => {
         if (!userId) {
@@ -52,22 +53,44 @@ const BranchUserScreen = () => {
         const fetchBranchDetails = async () => {
             const branchIds = await getBranchesForUser();
             branchIds.forEach(branchId => {
-                getBranchDetails(branchId); // Lấy thông tin chi tiết cho mỗi chi nhánh
+                getBranchDetails(branchId);
             });
         };
 
         fetchBranchDetails();
     }, []);
 
+    const handleLogout = () => {
+        Alert.alert(
+          "Đăng Xuất",
+          "Bạn có chắc chắn muốn đăng xuất không?",
+          [
+            {
+              text: "Hủy",
+              onPress: () => console.log("Hủy đăng xuất"),
+              style: "cancel"
+            },
+            { 
+              text: "Đăng Xuất", 
+              onPress: () => {
+                console.log("Tiến hành đăng xuất");
+                setAuthToken(null);
+                dispatch(actions.setAccessToken(null));
+                navigation.navigate('AuthStack');
+              }
+            }
+          ]
+        );
+      };
+      
     const renderItem = ({ item }) => (
         <TouchableOpacity
             style={styles.branchItem}
-            // Thêm sự kiện onPress để chuyển hướng và truyền dữ liệu
             onPress={() => navigation.navigate('MAIN_HomeScreen', {
                 branchDetails: branchDetails[item]
             })}
         >
-            <Text style={{ fontWeight: 'bold', fontSize: 18 }}>
+            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>
                 Bãi đỗ xe: {branchDetails[item]?.name}
             </Text>
             <Text>Địa chỉ: {branchDetails[item]?.address}</Text>
@@ -84,6 +107,9 @@ const BranchUserScreen = () => {
                 textColor={'#fff'}
                 leftComponent
                 isHome
+                showLogoutButton={true}
+                onRightPress={handleLogout}
+                rightIcon="sign-out-alt"
             />
             <FlatList
                 data={Object.keys(branchDetails)}
